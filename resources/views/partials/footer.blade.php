e6bc548aef6ebd5c0ec3f8d74bca29314ddd6723
MarioBlasi <mblasi509@gmail.com>
2023-05-29 16:50:26 +0100

    404 - 18

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="container-flex mt-5 bg-dark text-white d-flex justify-content-center p-5">
+        <div class="row ">
+                <div class="card-header">
+                  Featured
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="container-flex bg-dark text-white pt-3 d-flex justify-content-center p-5">
+        <div class="row ">
+            <div class="col-12">
+                    <div class="card-header">
+                      <ul class="nav nav-pills card-header-pills">
+                        <li class="nav-item">
+                          <a class="nav-link active" href="#">Active</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link ">Disabled</a>
+                        </li>
+                      </ul>
+                    </div>
+            </div>
+        </div>
+    </div>
+</header>
